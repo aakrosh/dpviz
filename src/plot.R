13b95f5bbabd6761650d7f6e2e456c0659081c48
Aakrosh Ratan <aakrosh@users.noreply.github.com>
2016-12-02 17:57:53 -0500

    added support to handle larger number of samples

--- src/plot.R
+++ src/plot.R
@@ -12,12 +12,23 @@ data = read.table(input, header = T)
 data = data[,-1]
 data = melt(data, id.vars = c("POS"))
 
+# maximum coverage I should show in the figure
+maxcov = quantile(data$value, 0.99)
+
 p = ggplot(data, aes(POS, value, group = variable, color = variable)) +
-    geom_smooth(span = 0.1) + 
-    geom_point(alpha = 0.5) +
+    geom_smooth(method = "loess", span = 0.1) + 
     xlab("position on chromosome") +
     ylab("depth on snps") + 
-    ggtitle("position vs depth") 
-pdf(output)
+    ggtitle("position vs depth") +
+    ylim(0,maxcov)
+
+# if there are more than 10 samples, do not create a legend
+if (length(unique(data$variable)) > 10) {
+    p = p + theme(legend.position="none")
+} else {
+    p = p + geom_point(alpha = 0.1)
+}   
+
+png(output, width = 960, height = 480)
 p
 dev.off()
